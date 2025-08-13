5d1d6f9fc0b0596cfd149d9da980ade62195df71
JhonGonzzzales <jhonherbertgonzaleshurtado@gmail.com>
2025-08-12 22:20:18 -0400

    Primera versión

---
+++ clase01.py
@@ -0,0 +1,26 @@
+print("Hola mundo desde Python\n")
+
+# Variable de tipo cadena o string
+nombre = "Bruno Diaz"
+# Variable de tipo entero
+edad = 30
+# Variable de tipo flotante
+altura = 1.75
+# Variable de tipo booleano
+es_estudiante = True
+
+# Imprimir las variables (tradicional)
+print("Impresión tradicional")
+print("---------------------")
+print("Nombre: ",nombre)
+print("Edad: ",edad)
+print("Altura: ",edad)
+print("Es estudiante: ",es_estudiante)
+
+# Imprimir las variables (moderna f-strings)
+print("Impresión moderna con f-strings")
+print("-------------------------------")
+print(f"Nombre: {nombre} ")
+print(f"Edad: {edad} ")
+print(f"Altura: {altura} ")
+print(f"Es estudiante: {es_estudiante} ")
